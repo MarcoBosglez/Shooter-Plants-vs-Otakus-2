44720fbe90ecc7471727689b0a42508b91565246
Marco Bosquez <60762402+MarcoBosglez@users.noreply.github.com>
2020-02-06 19:53:33 -0600

    Update Attack.cs

--- Attack.cs
+++ Attack.cs
@@ -1,1 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Attack : MonoBehaviour
+{
+
+    [Range(0f, 10f)]
+    [SerializeField] float speed = 1f;
+  
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+}
